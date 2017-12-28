7a60b4c9969c901f24aabef6b2bb565e74456d53
Abhinav Kasamsetty <abhinavkasamsetty@gmail.com>
2017-12-28 16:03:26 -0600

    Finished movement methods for all faces and added simple text UI

--- solver.py
+++ solver.py
@@ -14,63 +14,65 @@ class Cube(object):
                      'b':[['b']*3 for i in range(3)],
                      'u':[['w']*3 for i in range(3)],
                      'd':[['y']*3 for i in range(3)]}
+        self.parse_randomizer(randomizer)
 
+    #Goes through the randomizer string to change the cube state move by move
+    def parse_randomizer(self, randomizer):
         #Checks if the randomizer String is valid by WCA standards
         if randomizer is not None:
             if isinstance(randomizer, str) and len(randomizer) is not 0:
                 lower = randomizer.lower()
                 trans = lower.translate(dict.fromkeys(map(ord, ' \'lfrbud2'), None))
                 if len(trans) is 0:
-                    self.__parse_randomizer(lower)
+                    arr = randomizer.strip().split(' ')
+                    for move in arr:
+                        if move == 'l':
+                            self.l()
+                        elif move == 'l\'':
+                            self.l_prime()
+                        elif move == 'l2':
+                            self.l_2()
+                        elif move == 'f':
+                            self.f()
+                        elif move == 'f\'':
+                            self.f_prime()
+                        elif move == 'f2':
+                            self.f_2()
+                        elif move == 'r':
+                            self.r()
+                        elif move == 'r\'':
+                            self.r_prime()
+                        elif move == 'r2':
+                            self.r_2()
+                        elif move == 'b':
+                            self.b()
+                        elif move == 'b\'':
+                            self.b_prime()
+                        elif move == 'b2':
+                            self.b_2()
+                        elif move == 'u':
+                            self.u()
+                        elif move == 'u\'':
+                            self.u_prime()
+                        elif move == 'u2':
+                            self.u_2()
+                        elif move == 'd':
+                            self.d()
+                        elif move == 'd\'':
+                            self.d_prime()
+                        elif move == 'd2':
+                            self.d_2()
+                        else:
+                            print('Reverted to solved state as randomizer could not be parsed correctly')
+                            self.dict = dict(Cube.solved_cube)
                 else:
-                    print('String must only have valid cubing moves and one ' +
-                        'space between characters') 
+                    print('Cube state unchanged as randomizer was not in valid WCA format') 
             else:
-                print('Must send in a String of length greater than 0')
-
-    #Goes through the randomizer string to change the cube state move by move
-    def __parse_randomizer(self, randomizer):
-        arr = randomizer.split(' ')
-        for move in arr:
-            if move == 'l':
-                self.l()
-            elif move == 'l\'':
-                self.l_prime()
-            elif move == 'l2':
-                self.l_2()
-            elif move == 'f':
-                self.f()
-            elif move == 'f\'':
-                self.f_prime()
-            elif move == 'f2':
-                self.f_2()
-            # elif move == 'r':
-            #     self.r()
-            # elif move == 'r\'':
-            #     self.r_prime()
-            # elif move == 'r2':
-            #     self.r_2()
-            # elif move == 'b':
-            #     self.b()
-            # elif move == 'b\'':
-            #     self.b_prime()
-            # elif move == 'b2':
-            #     self.l_2()
-            # elif move == 'u':
-            #     self.u()
-            # elif move == 'u\'':
-            #     self.u_prime()
-            # elif move == 'u2':
-            #     self.u_2()
-            # elif move == 'd':
-            #     self.d()
-            # elif move == 'd\'':
-            #     self.d_prime()
-            # else:
-            #     self.d_2()
+                print('Cube state unchanged as no randomizer was passed in')
 
     #Solve the cube
-    # def solve(self):
+    def solve(self):
+        print('Solver not written yet.')
     #     arr = ['l', 'l\'', 'l2', 'f', 'f\'', 'f2', 'r', 'r\'', 'r2',
     #            'b', 'b\'', 'b2', 'u', 'u\'', 'u2', 'd', 'd\'', 'd2']            
 
@@ -136,33 +138,121 @@ class Cube(object):
             self.dict['u'][2][i], self.dict['d'][0][2 - i] = self.dict['d'][0][2 - i], self.dict['u'][2][i]
             self.dict['r'][i][0], self.dict['l'][2 - i][2] = self.dict['l'][2 - i][2], self.dict['r'][i][0]
 
-    # # Moving right side of cube 
-    # def r(self):
+    # Moving right side of cube 
+    def r(self):
+        self.__rotate_clock('r')
+        faces = ['u', 'f', 'd', 'b']
+        temp_arr = []
+        for i in range(3):
+            temp_arr.append(self.dict['u'][i][2])
+        for j in range(3):
+            for i in range(3):
+                self.dict[faces[j]][i][2] = self.dict[faces[j + 1]][i][2]
+        for i in range(3):
+            self.dict['b'][i][2] = temp_arr[i]
 
-    # def r_prime(self):
+    def r_prime(self):
+        self.__rotate_counter('r')
+        faces = ['u', 'b', 'd', 'f']
+        temp_arr = []
+        for i in range(3):
+            temp_arr.append(self.dict['u'][i][2])
+        for j in range(3):
+            for i in range(3):
+                self.dict[faces[j]][i][2] = self.dict[faces[j + 1]][i][2]
+        for i in range(3):
+            self.dict['f'][i][2] = temp_arr[i]
 
-    # def r_2(self):
+    def r_2(self):
+        self.__rotate__2('r')
+        for i in range(3):
+            self.dict['u'][i][2], self.dict['d'][i][2] = self.dict['d'][i][2], self.dict['u'][i][2]
+            self.dict['f'][i][2], self.dict['b'][i][2] = self.dict['b'][i][2], self.dict['f'][i][2]
 
-    # # Moving back side of cube  
-    # def b(self):
+    # Moving back side of cube  
+    def b(self):
+        self.__rotate_clock('b')
+        temp_arr = list(self.dict['u'][0])
+        for i in range(3):
+            self.dict['u'][0][i] = self.dict['r'][i][2]
+        for i in range(3):
+            self.dict['r'][i][2] = self.dict['d'][2][2 - i]
+        for i in range(3):
+            self.dict['d'][2][2 - i] = self.dict['l'][2 - i][0]
+        for i in range(3):
+            self.dict['l'][2 - i][0] = temp_arr[i]
 
-    # def b_prime(self):
+    def b_prime(self):
+        self.__rotate_counter('b')
+        temp_arr = list(self.dict['u'][0])
+        for i in range(3):
+            self.dict['u'][0][i] = self.dict['l'][2 - i][0]
+        for i in range(3):
+            self.dict['l'][2 - i][0] = self.dict['d'][2][2 - i]
+        for i in range(3):
+            self.dict['d'][2][2 - i] = self.dict['r'][i][2]
+        for i in range(3):
+            self.dict['r'][i][2] = temp_arr[i]
 
-    # def b_2(self):
+    def b_2(self):
+        self.__rotate__2('b')
+        for i in range(3):
+            self.dict['u'][0][i], self.dict['d'][2][2 - i] = self.dict['d'][2][2 - i], self.dict['u'][0][i]
+            self.dict['r'][i][2], self.dict['l'][2 - i][0] = self.dict['l'][2 - i][0], self.dict['r'][i][2]
 
-    # # Moving upper side of cube 
-    # def u(self):
+    # Moving upper side of cube 
+    def u(self):
+        self.__rotate_clock('u')
+        temp = list(self.dict['f'][0])
+        self.dict['f'][0] = list(self.dict['r'][0])
+        for i in range(3):
+            self.dict['r'][0][i] = self.dict['b'][2][2 - i]
+        for i in range(3):
+            self.dict['b'][2][2 - i] = self.dict['l'][0][2 - i]
+        self.dict['l'][0] = temp
 
-    # def u_prime(self):
+    def u_prime(self):
+        self.__rotate_counter('u')
+        temp = list(self.dict['f'][0])
+        self.dict['f'][0] = list(self.dict['l'][0])
+        for i in range(3):
+            self.dict['l'][0][i] = self.dict['b'][2][2 - i]
+        for i in range(3):
+            self.dict['b'][2][i] = self.dict['r'][0][2 - i]
+        self.dict['r'][0] = temp
 
-    # def u_2(self):
+    def u_2(self):
+        self.__rotate__2('u')
+        self.dict['l'][0], self.dict['r'][0] = self.dict['r'][0], self.dict['l'][0]
+        for i in range(3):
+            self.dict['f'][0][i], self.dict['b'][2][2 - i] = self.dict['b'][2][2 - i], self.dict['f'][0][i]
 
-    # # Moving down side of cube  
-    # def d(self):
+    # Moving down side of cube  
+    def d(self):
+        self.__rotate_clock('d')
+        temp = list(self.dict['f'][2])
+        self.dict['f'][2] = list(self.dict['l'][2])
+        for i in range(3):
+            self.dict['l'][2][i] = self.dict['b'][0][2 - i]
+        for i in range(3):
+            self.dict['b'][0][2 - i] = self.dict['r'][2][i]
+        self.dict['r'][2] = temp
 
-    # def d_prime(self):
+    def d_prime(self):
+        self.__rotate_counter('d')
+        temp = list(self.dict['f'][2])
+        self.dict['f'][2] = list(self.dict['r'][2])
+        for i in range(3):
+            self.dict['r'][2][i] = self.dict['b'][0][2 - i]
+        for i in range(3):
+            self.dict['b'][0][2 - i] = self.dict['l'][2][i]
+        self.dict['l'][2] = temp
 
-    # def d_2(self):
+    def d_2(self):
+        self.__rotate__2('d')
+        self.dict['l'][2], self.dict['r'][2] = self.dict['r'][2], self.dict['l'][2]
+        for i in range(3):
+            self.dict['f'][2][i], self.dict['b'][0][2 - i] = self.dict['b'][0][2 - i], self.dict['f'][2][i]
 
     #Rotate face clockwise
     def __rotate_clock(self, face):
@@ -214,8 +304,21 @@ class Cube(object):
             print('      ' + mat[i][0] + ' ' + mat[i][1] + ' ' + mat[i][2])
 
 def main():
-    rubik = Cube(input('Enter cube randomizer string in WCA format. '))
+    rubik = Cube(input('Enter cube randomizer string in WCA format: '))
     rubik.display()
+    print('\nOptions:\n1 to solve\n2 to create new cube\n3 to enter randomizer string\nAnything else to exit')
+    choice = input('Enter input here: ')
+    while (choice == '1' or choice == '2' or choice == '3'):
+        print()
+        if choice == '1':
+            rubik.solve()
+        elif choice == '2':
+            rubik = Cube(input('Enter cube randomizer string in WCA format: '))
+        else:
+            rubik.parse_randomizer(input('Enter cube randomizer string in WCA format: '))
+        rubik.display()
+        print('\nOptions:\n1 to solve\n2 to create new cube\n3 to enter randomizer string\nAnything else to exit')
+        choice = input('Enter input here: ')
 
 if __name__ == '__main__':
     main()
